40a78e3a56a87e0c2a1efd82010c52d66bdc324b
Tim Stott <ta.stott@googlemail.com>
2015-12-04 23:21:14 +0000

    Working test

--- Code/app.ts
+++ Code/app.ts
@@ -1,37 +1,43 @@
-declare function global(varName: string): any;
 
-export function ComposePingResponse(): string {
-	var latLngPattern = /([^,]+),(.+)/;
-	var maxAge = 1200; //20 mins
+export class PingResponder {
+	constructor(private getGlobal: (varName: string) => any) {
 
-	var bestLocation = [
-		{ LatLngVar: "%LOC", TimeVar: "%LOCTMS" },
-		{ LatLngVar: "%LOCN", TimeVar: "%LOCNTMS" }
-	]
-		.map(l => {
-			var latLngMatch: RegExpMatchArray;
-			if (global(l.LatLngVar) && (latLngMatch = global(l.LatLngVar).match(latLngPattern))) {
-				var age = global(l.TimeVar) - global("%TIMES");
-				return {
-					Lat: latLngMatch[1],
-					Lng: latLngMatch[2],
-					Age: age
-				};
-			}
-			else {
-				return null;
-			}
-		})
-		.filter(l => !!l)
-		.filter(l => l.Age < maxAge)
-		.sort((a, b) => a.Age - b.Age)
-	[0];
-
-	if (bestLocation) {
-		var gmapsUrl = `http;//maps.google.com/maps?z=12&t=m&q=loc:${bestLocation.Lat}+${bestLocation.Lng}`;
-		return gmapsUrl;
 	}
-	else {
-		return "Unable to get location";
+
+
+	ComposePingResponse(): string {
+		var latLngPattern = /([^,]+),(.+)/;
+		var maxAge = 1200; //20 mins
+
+		var bestLocation = [
+			{ LatLngVar: "%LOC", TimeVar: "%LOCTMS" },
+			{ LatLngVar: "%LOCN", TimeVar: "%LOCNTMS" }
+		]
+			.map(l => {
+				var latLngMatch: RegExpMatchArray;
+				if (this.getGlobal(l.LatLngVar) && (latLngMatch = this.getGlobal(l.LatLngVar).match(latLngPattern))) {
+					var age = this.getGlobal(l.TimeVar) - this.getGlobal("%TIMES");
+					return {
+						Lat: latLngMatch[1],
+						Lng: latLngMatch[2],
+						Age: age
+					};
+				}
+				else {
+					return null;
+				}
+			})
+			.filter(l => !!l)
+			.filter(l => l.Age < maxAge)
+			.sort((a, b) => a.Age - b.Age)
+		[0];
+
+		if (bestLocation) {
+			var gmapsUrl = `http;//maps.google.com/maps?z=12&t=m&q=loc:${bestLocation.Lat}+${bestLocation.Lng}`;
+			return gmapsUrl;
+		}
+		else {
+			return "Unable to get location";
+		}
 	}
 }
--- Code/test/app.ts
+++ Code/test/app.ts
@@ -7,6 +7,17 @@ import app = require("../app")
 
 describe("Example app tests", () => {
 	it("Should do stuff", () => {
-		var response = app.ComposePingResponse();
+		var responder = new app.PingResponder(varName => {
+			switch(varName){
+				case "%LOC": return "51,1.3";
+				case "%LOCTMS": return 12345678;
+				case "%LOCN": return "53,-1.3";
+				case "%LOCNTMS": return 12345679;
+				case "%LOCSPD": return 10;
+				case "%TIMES": return 12345677;
+				default: throw new Error(`Unrecognized variable name: '${varName}'`);
+			}
+		});
+		var response = responder.ComposePingResponse();
 	})
 })
